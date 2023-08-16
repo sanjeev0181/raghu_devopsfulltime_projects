fd0f70a65b32819758defa4b9893810527e7e351
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-16 22:48:45 +0530

    adding the file in mongodb

--- shell_script/02-mongodb.sh
+++ shell_script/02-mongodb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+sudo rpm --import https://www.mongodb.org/static/pgp/server-5.0.asc
 cp vim /etc/yum.repos.d/mongo.repo
 sudo yum makecache
 sudo yum install -y mongodb-org
-systemctl enable mongod 
-systemctl start mongod 
+systemctl enable mongodb
+systemctl start mongodb 
 #127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-systemctl restart mongod
+systemctl restart mongodb
