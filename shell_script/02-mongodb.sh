0012a8a91c30e47416d7f5e712450140585a27c4
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-16 21:21:23 +0530

    adding the file in mongodb

--- shell_script/02-mongodb.sh
+++ shell_script/02-mongodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp vim /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y 
+sudo yum makecache
+sudo yum install -y mongodb-org
 systemctl enable mongod 
 systemctl start mongod 
 #127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
