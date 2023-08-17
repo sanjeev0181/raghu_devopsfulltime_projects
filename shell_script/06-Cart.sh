9d9110a930f00681e55ea7f21cc3017e0ede54b8
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-17 12:33:20 +0530

    adding the npm installation step in 06 card

--- shell_script/06-Cart.sh
+++ shell_script/06-Cart.sh
@@ -7,7 +7,10 @@ curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app 
 unzip /tmp/cart.zip
 cd /app 
-sudo amazon-linux-extras install nodejs12 -y
+#sudo amazon-linux-extras install nodejs12 -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
 cp /root/raghu_devopsfulltime_projects/shell_script/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
 systemctl enable cart 
