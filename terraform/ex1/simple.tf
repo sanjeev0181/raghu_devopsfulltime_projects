9404a000080cb9486b8ea0f2029a1335379e41cf
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-18 14:14:31 +0530

    adding simple file in data

--- terraform/ex1/simple.tf
+++ terraform/ex1/simple.tf
@@ -1,8 +1,24 @@
-resource "aws_instance" "web" {
-  ami           = "ami-024e6efaf93d85776"
-  instance_type = "t2.micro"
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-  tags = {
-    Name = "HelloWorld"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]  # Update the filter pattern for the desired Ubuntu version
   }
+
+  owners = ["513442679011"]  # Canonical's AWS account ID for official Ubuntu AMIs
 }
+
+output "ami" {
+  value = data.aws_ami.ubuntu.id
+  
+}
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-024e6efaf93d85776"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
