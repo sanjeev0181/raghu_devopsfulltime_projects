aaeb1335c30ff50e4f296792e82a37218092f6b9
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-21 17:42:14 +0530

    adding the null resources

--- terraform/03-loops/count/main.tf
+++ terraform/03-loops/count/main.tf
@@ -2,10 +2,10 @@ resource "null_resource" "null" {
   count = 10
 }
 
-resource "null_resource" "null1" {
+resource "null_resource" "fruit" {
   count = length(var.list)
 }
 
-variable "list" {
+variable "fruit" {
   default = ["banana", "apple", "organe"]
 }
