a3728e9448bbdb9dc3b4d68f3ec88859ad83584a
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-09-05 15:00:56 +0530

    adding to module in ec2 and sg

---
+++ terraform/ex2/sg/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "allow_tls" {
+  name = "allow-tls"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allows-tls"
+  }
+}
+
+output "sg_id" {
+    value = "aws_security_group.allows_tls.id"
+}
